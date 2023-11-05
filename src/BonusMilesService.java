d807640cc5ba2211cc77a312e93a93448b9a13bb
Vladimir <garulikvovka97@gmail.com>
2023-11-05 21:46:45 +0300

    first commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,9 @@
+public class BonusMilesService {
+
+    public int calculate(int price) {
+
+        int miles = price / 20; // расчет значений стоимость деленая на 20
+
+        return miles;
+    }
+}
